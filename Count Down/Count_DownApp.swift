2795cbe725ce909a7639387e1d030b51571d4f48
kraiglarson2 <kraigalarson2@gmail.com>
2023-12-12 13:12:16 -0500

    Initial Commit

---
+++ Count Down/Count_DownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Count_DownApp.swift
+//  Count Down
+//
+//  Created by Krajg Larson on 12/12/23.
+//
+
+import SwiftUI
+
+@main
+struct Count_DownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
